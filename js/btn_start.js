ead7b98603a8c2724bea8329f39a12d29184752f
Vicente Bravo <vicentebravo22131@gmail.com>
2025-07-21 15:42:07 -0600

    Creamos un botón para navegar a otra sección

--- js/btn_start.js
+++ js/btn_start.js
@@ -1,2 +1,14 @@
 // Qué hara este botón
 // El botón debe navegar a la página que esta dentro de mi carpeta html en el archivo game.html
+
+// Espera que todo el documento esté cargado
+window.addEventListener('DOMContentLoaded', () => {
+    // Obtiene el botón por su ID
+    const startButton = document.getElementById('starGame');
+
+    // Agrega un evento de Click al botón
+    startButton.addEventListener('click', () => {
+        // Llamamos la función para rederijir a la página del juego
+        redirectTo('html/game.html');
+    });
+})
