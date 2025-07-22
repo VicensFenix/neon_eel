23cdcc954dd3aaa9544b2d2f3a0796da93eafce3
Vicente Bravo <vicentebravo22131@gmail.com>
2025-07-22 15:35:01 -0600

    Agregando instucciones

---
+++ js/player_pj.js
@@ -0,0 +1,5 @@
+// Usaremos programación orientada a objetos para crear al jugador(anguila)
+// Lo debo de mudalizar 
+// 1. El personaje si choca con los boderdes del contenedor, cambia de dirección
+// 2. El personaje si come la comida, aumenta de tamaño y esa comida desaparece
+// 3. El personaje si choca con su cuerpo, termina el juego
