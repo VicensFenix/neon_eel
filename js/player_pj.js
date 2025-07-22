3389c9bfd33c5ebbfd7927e49fb7380a918ec35a
Vicente Bravo <vicentebravo22131@gmail.com>
2025-07-22 16:15:57 -0600

    xd

--- js/player_pj.js
+++ js/player_pj.js
@@ -1,9 +1,3 @@
-// Usaremos programación orientada a objetos para crear al jugador(anguila)
-// Lo debo de mudalizar 
-// 1. El personaje si choca con los boderdes del contenedor, cambia de dirección
-// 2. El personaje si come la comida, aumenta de tamaño y esa comida desaparece
-// 3. El personaje si choca con su cuerpo, termina el juego
-
 // Clase que define al jugador
 export class Player {
     // ✅ Definimos las propiedades del jugador
